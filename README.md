4a550e3b6c14bdfb5aa6bc06ef30eff8e7c10086
atharvnegi <atharvnegi2951@gmail.com>
2022-03-14 17:06:58 +0000

    Add extras for a6ltedx-user 10 QP1A.190711.020 A600GDXU9CUC2 release-keys
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## a6ltedx-user 10 QP1A.190711.020 A600GDXU9CUC2 release-keys
+- Manufacturer: samsung
+- Platform: universal7870
+- Codename: a6lte
+- Brand: samsung
+- Flavor: a6ltedx-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: A600GDXU9CUC2
+- Tags: release-keys
+- CPU Abilist: armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 320
+- Fingerprint: samsung/a6ltedx/a6lte:10/QP1A.190711.020/A600GDXU9CUC2:user/release-keys
+- OTA version: 
+- Branch: a6ltedx-user-10-QP1A.190711.020-A600GDXU9CUC2-release-keys
+- Repo: samsung_a6lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/oat/arm/GmsCore.odex.* 2>/dev/null >> system/system/priv-app/GmsCore/oat/arm/GmsCore.odex
+rm -f system/system/priv-app/GmsCore/oat/arm/GmsCore.odex.* 2>/dev/null
+cat system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
